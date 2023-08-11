b62ff11171c2eeded8bc1fd85ac5fc606a8e9785
knoori-code <knoori3@gmail.com>
2023-08-11 16:38:21 -0400

    Create header variable for readability

--- 17 Dicee Challenge/index.js
+++ 17 Dicee Challenge/index.js
@@ -4,10 +4,11 @@ document.querySelector(".img1").setAttribute("src", `./images/dice${randomNumber
 let randomNumber2 = Math.floor((Math.random() * 6) + 1);
 document.querySelector(".img2").setAttribute("src", `./images/dice${randomNumber2}.png`);
 
+const header = document.querySelector("h1")
 if (randomNumber1 < randomNumber2) {
-  document.querySelector("h1").textContent = "Player 2 Wins!"
+  header.textContent = "Player 2 Wins!"
 } else if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").textContent = "Player 1 Wins!"
+  header.textContent = "Player 1 Wins!"
 } else {
-  document.querySelector("h1").textContent = "The Game is a Draw!"
+  header.textContent = "The Game is a Tie!"
 }
