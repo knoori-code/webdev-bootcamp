72124fdb8c63d2df56d9e3e6108eb5ef51aa4873
knoori-code <knoori3@gmail.com>
2023-08-13 21:22:24 -0400

    Create makeSound function

--- 18 Drum Kit Starting Files/index.js
+++ 18 Drum Kit Starting Files/index.js
@@ -2,54 +2,16 @@ const listOfButtons = document.querySelectorAll(".drum");
 
 for (let button of listOfButtons) {
   button.addEventListener("click", function () {
-    let buttonInnerHTML = this.innerHTML;
-
-    switch (buttonInnerHTML) {
-      case "w":
-        const crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-
-      case "a":
-        const kickBass = new Audio("sounds/kick-bass.mp3");
-        kickBass.play();
-        break;
-
-      case "s":
-        const snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-
-      case "d":
-        const tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-
-      case "j":
-        const tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-
-      case "k":
-        const tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-
-      case "l":
-        const tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
-
-      default:
-        console.log(buttonInnerHTML);
-    }
+    makeSound(this.innerHTML);
   });
 }
 
 document.addEventListener("keydown", (event) => {
-  let keyPressed = event.key;
+  makeSound(event.key);
+});
 
-  switch (keyPressed) {
+function makeSound(key) {
+  switch (key) {
     case "w":
       const crash = new Audio("sounds/crash.mp3");
       crash.play();
@@ -86,5 +48,6 @@ document.addEventListener("keydown", (event) => {
       break;
 
     default:
+      console.log(buttonInnerHTML);
   }
-});
+}
