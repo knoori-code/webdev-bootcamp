903d4dd94c4549b171ac58e90ed8b25f5ca1d68b
knoori-code <knoori3@gmail.com>
2023-08-13 20:52:20 -0400

    Complete switch statement for all key presses

--- 18 Drum Kit Starting Files/index.js
+++ 18 Drum Kit Starting Files/index.js
@@ -44,21 +44,47 @@ for (let button of listOfButtons) {
         console.log(buttonInnerHTML);
     }
   });
-
 }
 
 document.addEventListener("keydown", (event) => {
-  let keyPressed = event.key
+  let keyPressed = event.key;
 
   switch (keyPressed) {
     case "w":
       const crash = new Audio("sounds/crash.mp3");
       crash.play();
       break;
-  
-    default:
-      break;
-  }
 
-  
-})
+    case "a":
+      const kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+
+    case "s":
+      const snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "d":
+      const tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "j":
+      const tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "k":
+      const tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "l":
+      const tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    default:
+  }
+});
