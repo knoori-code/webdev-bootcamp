ff187580b264146efe04f908f29dcb89d78206ba
knoori-code <knoori3@gmail.com>
2023-09-07 20:42:30 -0400

    Install and use superheroes package

--- Backend/2.3 NPM/index.js
+++ Backend/2.3 NPM/index.js
@@ -1,6 +1,12 @@
 // const generateName = require('sillyname');
 
-import generateName from "sillyname"
-const sillyName = generateName();
+// import generateName from "sillyname"
+// const sillyName = generateName();
 
-console.log(`My name is ${sillyName}`);
+// console.log(`My name is ${sillyName}`);
+
+import superheroes from "superheroes";
+
+const heroName = superheroes.random();
+console.log(`My name is the great ${heroName}!`);
+
