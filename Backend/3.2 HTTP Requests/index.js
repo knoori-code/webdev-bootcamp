138dd9df1e9f89bf785b72bba45287fb0ff2bcc1
knoori-code <knoori3@gmail.com>
2023-09-08 20:41:22 -0400

    Add more get endpoints

--- Backend/3.2 HTTP Requests/index.js
+++ Backend/3.2 HTTP Requests/index.js
@@ -3,9 +3,17 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  
-})
+  res.send("<h1>Hello World!</>");
+});
+
+app.get("/contacts", (req, res) => {
+  res.send("<h1>This is the contacts page</>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>This is the about page</>");
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
