a54a8dfa6e9582c569d23bc6e6f5d322a4cdc89c
knoori-code <knoori3@gmail.com>
2023-09-10 23:30:42 -0400

    Finish name generator program

--- Backend/3.4 Middleware/index4.js
+++ Backend/3.4 Middleware/index4.js
@@ -1,7 +1,26 @@
 import express from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  let generatedName = req.body.street + req.body.pet;
+  console.log(generatedName);
+  res.send(`<h1>Your band name is:</><h2>${generatedName}</>`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
