a72a1fe2f690a0b1072d24d62c620073dedb1c12
knoori-code <knoori3@gmail.com>
2023-09-12 15:29:56 -0400

    Import express and body-parser, set up server

--- Backend/3.5 Secrets Project/index.js
+++ Backend/3.5 Secrets Project/index.js
@@ -1,3 +1,23 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+
+import express from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+
+
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
+
+app.get("/", (req, res) => {
+  res.sendFile()  
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
