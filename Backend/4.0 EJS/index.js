2430dc17c118c9a8085488b820c4a68b492c381e
knoori-code <knoori3@gmail.com>
2023-09-12 17:22:33 -0400

    Complete activity

--- Backend/4.0 EJS/index.js
+++ Backend/4.0 EJS/index.js
@@ -4,11 +4,19 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  const day = new Date();
-  console.log(day);
-  res.render("index.ejs", {})
-})
+  const date = new Date();
+  let day = date.getDay();
+  let message = ""
+
+  if (day === 0 || day === 6) {
+    message = "Hey!  It's the weekend, it's time to have fun!"
+  } else {
+    message = "Hey!  It's a weekday, time to work hard!"
+  }
+
+  res.render("index.ejs", { message: message });
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
-})
+});
