4ff9a77ee29eb4714e435d89ea12aed59154a710
knoori-code <knoori3@gmail.com>
2023-09-12 15:35:58 -0400

    Complete secrets program

--- Backend/3.5 Secrets Project/index.js
+++ Backend/3.5 Secrets Project/index.js
@@ -6,7 +6,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 
-
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
@@ -15,8 +17,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
-  res.sendFile()  
-})
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+  if (req.body.password === "ILoveProgramming") {
+    res.sendFile(__dirname + "/public/secret.html");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
