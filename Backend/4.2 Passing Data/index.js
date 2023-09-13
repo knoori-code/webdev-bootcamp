098885b07daa3b62e6f5f5a8b8da11b019ff91b6
knoori-code <knoori3@gmail.com>
2023-09-13 17:59:53 -0400

    Complete challenge

--- Backend/4.2 Passing Data/index.js
+++ Backend/4.2 Passing Data/index.js
@@ -3,12 +3,35 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+let combinedName = "";
+
+function name(req, res, next) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  combinedName = firstName + lastName;
+  next();
+}
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(name);
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  let msg = "Enter your name below";
+  res.render("index.ejs", { message: msg });
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const fullNameLength = combinedName.length;
+  let msg;
+  
+  if (fullNameLength !== 0) {
+    msg = `There are ${fullNameLength} letters in your name`;
+  } else {
+    msg = "Please enter a valid name";
+  }
+
+  res.render("index.ejs", { message: msg });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
