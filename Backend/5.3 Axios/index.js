55f8b827245e351921623c84589e6b3ed028364b
knoori-code <knoori3@gmail.com>
2023-09-17 19:10:57 -0400

    Add Axios call to post route

--- Backend/5.3 Axios/index.js
+++ Backend/5.3 Axios/index.js
@@ -36,6 +36,17 @@ app.post("/", async (req, res) => {
   // Step 3: If you get a 404 error (resource not found) from the API request.
   // Pass an error to the index.ejs to tell the user:
   // "No activities that match your criteria."
+
+  try {
+    const response = await axios.get(
+      `https://bored-api.appbrewery.com/filter?type=${req.body.type}&participants=${req.body.participants}`
+    );
+    const result = response.data;
+    res.render("index.ejs", { data: result });
+  } catch (error) {
+    console.error("Failed to make request: ", error.message);
+    res.render("index.ejs", { error: error.message });
+  }
 });
 
 app.listen(port, () => {
