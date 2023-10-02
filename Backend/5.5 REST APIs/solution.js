23408232809194d1e69d2ead022c922063ede738
knoori-code <knoori3@gmail.com>
2023-10-02 15:26:02 -0400

    Enter bearer token into solution.js

--- Backend/5.5 REST APIs/solution.js
+++ Backend/5.5 REST APIs/solution.js
@@ -7,7 +7,7 @@ const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
 //Add your own bearer token from the previous lesson.
-const yourBearerToken = "08f3026d-9c6c-4d88-a3b2-c579dc106247";
+const yourBearerToken = "37dcf546-d1e3-4942-ac12-feda02d85197";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
