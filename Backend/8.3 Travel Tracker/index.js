9f6e2a2fb225ed24f5a01fce762b825684b030cf
knoori-code <knoori3@gmail.com>
2023-10-29 14:01:01 -0400

    Set up pg client and make query for visited countries

--- Backend/8.3 Travel Tracker/index.js
+++ Backend/8.3 Travel Tracker/index.js
@@ -1,11 +1,32 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+const db = new pg.Client({
+  user: "khalidnoori",
+  host: "localhost",
+  database: "world",
+  port: 5432,
+});
+
+db.connect();
+const countries = [];
+
+db.query("SELECT * FROM visited_countries", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    countries = res.rows;
+  }
+
+  db.end();
+});
 
 app.get("/", async (req, res) => {
   //Write your code here.
