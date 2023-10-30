c63d9f4c774258346afb5d2adc68b83a2e22a203
knoori-code <knoori3@gmail.com>
2023-10-29 21:48:47 -0400

    Format code

--- Backend/8.3 Travel Tracker/index.js
+++ Backend/8.3 Travel Tracker/index.js
@@ -19,16 +19,16 @@ db.connect();
 
 app.get("/", async (req, res) => {
   let countries = [];
-  const result = await db.query("SELECT country_code FROM visited_countries")
+  const result = await db.query("SELECT country_code FROM visited_countries");
   countries = result.rows;
-  
+
   console.log("countries visited: ", countries);
   let numberVisited = countries.length;
-  
+
   //Write your code here.
   const countryArray = [];
   countries.forEach((country) => countryArray.push(country.country_code));
-  
+
   res.render("index.ejs", {
     total: numberVisited,
     countries: countryArray,
@@ -37,14 +37,18 @@ app.get("/", async (req, res) => {
 
 app.post("/add", async (req, res) => {
   const countryName = req.body.country;
-  const result = await db.query("SELECT country_code FROM countries WHERE country_name = $1", [countryName]);
-  const countryCodeArray = result.rows; 
-  console.log(countryCodeArray)
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1",
+    [countryName]
+  );
+  const countryCodeArray = result.rows;
+  console.log(countryCodeArray);
 
-  await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [countryCodeArray[0].country_code]);
-  res.redirect("/")
-
-})
+  await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+    countryCodeArray[0].country_code,
+  ]);
+  res.redirect("/");
+});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
