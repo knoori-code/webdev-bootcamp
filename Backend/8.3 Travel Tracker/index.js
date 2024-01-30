4051bb24da8921e5033048009df0efbfb23aec1a
knoori-code <knoori3@gmail.com>
2024-01-30 11:59:29 -0500

    Create connection to postgresql database

--- Backend/8.3 Travel Tracker/index.js
+++ Backend/8.3 Travel Tracker/index.js
@@ -1,14 +1,31 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "khalidnoori",
+  host: "localhost",
+  database: "world",
+  port: "5432",
+});
+
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   //Write your code here.
+
+  try {
+    const result = await db.query("SELECT * FROM countries");
+    console.log(result);
+  } catch (error) {
+    console.error("Error executing query", err.stack);
+  }
 });
 
 app.listen(port, () => {
