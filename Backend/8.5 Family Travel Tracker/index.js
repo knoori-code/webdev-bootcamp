41a69f9cbca32138c3df40fbc5a22be0c925c2eb
knoori-code <knoori3@gmail.com>
2024-02-04 15:15:20 -0500

    Add wildcards for searching country names

--- Backend/8.5 Family Travel Tracker/index.js
+++ Backend/8.5 Family Travel Tracker/index.js
@@ -61,7 +61,7 @@ app.post("/add", async (req, res) => {
 
   try {
     const result = await db.query(
-      "SELECT country_code FROM countries WHERE LOWER(country_name) = $1;",
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
       [input.toLowerCase()]
     );
 
