34e1bf192b6501b4c9e910071f5ab365e40c9b36
knoori-code <knoori3@gmail.com>
2024-03-07 20:18:49 -0500

    Install dependencies

--- 298-mapping-components/src/components/App.jsx
+++ 298-mapping-components/src/components/App.jsx
@@ -3,28 +3,21 @@ import Card from "./Card";
 import contacts from "../contacts";
 
 function App() {
+  const cardArray = contacts.map((contactObj) => {
+    return (
+      <Card
+        name={contactObj.name}
+        img={contactObj.imgURL}
+        tel={contactObj.phone}
+        email={contactObj.email}
+      />
+    );
+  });
+
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-
-      <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      />
+      {cardArray}
     </div>
   );
 }
