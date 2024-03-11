7d28ed01b0f4b9cf508ed011d04beeb7bf0426ab
knoori-code <knoori3@gmail.com>
2024-03-10 23:20:00 -0400

    Use setInterval to change time every second

--- 307-useState-hook-practice/src/components/App.jsx
+++ 307-useState-hook-practice/src/components/App.jsx
@@ -4,10 +4,16 @@ function App() {
   let time = new Date().toLocaleTimeString();
   const [currentTime, setCurrentTime] = useState(time);
 
+  setInterval(() => {
+    setCurrentTime(new Date().toLocaleTimeString());
+  }, 1000);
+
   return (
     <div className="container">
       <h1>{currentTime}</h1>
-      <button onClick={() => setCurrentTime(new Date().toLocaleTimeString())} >Get Time</button>
+      <button onClick={() => setCurrentTime(new Date().toLocaleTimeString())}>
+        Get Time
+      </button>
     </div>
   );
 }
