9c51910a8bd682f4d74e629c3043271a34ea50bc
knoori-code <knoori3@gmail.com>
2024-03-10 20:01:01 -0400

    Add button to increment count

--- 306-useState-hook/src/components/App.jsx
+++ 306-useState-hook/src/components/App.jsx
@@ -6,7 +6,8 @@ function App() {
   return (
     <div className="container">
       <h1>{count}</h1>
-      <button onClick={() => setCount((count - 1))}>+</button>
+      <button onClick={() => setCount((count + 1))}>+</button>
+      <button onClick={() => setCount((count - 1))}>-</button>
     </div>
   );
 }
