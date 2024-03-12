edad9b4b85b1322c4f837eb6cefcbe2223d76886
knoori-code <knoori3@gmail.com>
2024-03-11 23:38:45 -0400

    Use mouse over and mouse out event handlers to change button colour

--- 310-event-handling-in-react/src/components/App.jsx
+++ 310-event-handling-in-react/src/components/App.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  const [headingText, setHeadingText] = useState("Hello")
+  const [headingText, setHeadingText] = useState("Hello");
+
+  function handleClick() {
+    setHeadingText("Submitted");
+  }
+
+  function handleHover() {
+    document.getElementById("btn").style.backgroundColor = "black"
+  }
+
+  function handleMouseOut() {
+    document.getElementById("btn").style.backgroundColor = "white"
+  }
 
   return (
     <div className="container">
       <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button id="btn" onClick={handleClick} onMouseOver={handleHover} onMouseOut={handleMouseOut} >Submit</button>
     </div>
   );
 }
