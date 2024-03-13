2ed38f528300be520ff5c158e937bcbbe6bd6b5e
knoori-code <knoori3@gmail.com>
2024-03-12 20:20:55 -0400

    Create buttonColour state variable for mouseover and mouseout events

--- 310-event-handling-in-react/src/components/App.jsx
+++ 310-event-handling-in-react/src/components/App.jsx
@@ -2,24 +2,25 @@ import React, { useState } from "react";
 
 function App() {
   const [headingText, setHeadingText] = useState("Hello");
+  const [buttonColour, setButtonColour] = useState("white")
 
   function handleClick() {
     setHeadingText("Submitted");
   }
 
   function handleHover() {
-    document.getElementById("btn").style.backgroundColor = "black"
+    setButtonColour("black")
   }
 
   function handleMouseOut() {
-    document.getElementById("btn").style.backgroundColor = "white"
+    setButtonColour("white")
   }
 
   return (
     <div className="container">
       <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button id="btn" onClick={handleClick} onMouseOver={handleHover} onMouseOut={handleMouseOut} >Submit</button>
+      <button style={{backgroundColor: buttonColour}} onClick={handleClick} onMouseOver={handleHover} onMouseOut={handleMouseOut} >Submit</button>
     </div>
   );
 }
