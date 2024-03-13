01eb0a2becf6cd255ffab5f704e554f60f8a1830
knoori-code <knoori3@gmail.com>
2024-03-13 19:52:22 -0400

    Use 2 states to show typed first and last name

--- 313-changing-complex-state/src/components/App.jsx
+++ 313-changing-complex-state/src/components/App.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [secondName, setSecondName] = useState(""); 
+
+  function handleChange(event) {
+    setFirstName(event.target.value)
+  }
+
+  function handleSecondChange(event) {
+    setSecondName(event.target.value)
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {firstName} {secondName}</h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input name="fName" placeholder="First Name" onChange={handleChange} />
+        <input name="lName" placeholder="Last Name" onChange={handleSecondChange} />
         <button>Submit</button>
       </form>
     </div>
