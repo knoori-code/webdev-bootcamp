f765bcadb1402c23851e42a022827950b4cdac6e
knoori-code <knoori3@gmail.com>
2024-03-18 20:52:40 -0400

    Complete assignment

--- 316-es6-spread-operator-practice/src/components/App.jsx
+++ 316-es6-spread-operator-practice/src/components/App.jsx
@@ -8,6 +8,23 @@ function App() {
 
   function handleChange(event) {
     const { value } = event.target;
+
+    setState((prev) => {
+      return {
+        ...prev,
+        task: value
+      }
+    })
+  }
+
+  function handleClick() {
+    setState((prev) => {
+      prev.taskList.push(prev.task);
+
+      return {
+        ...prev
+      }
+    })
   }
 
   return (
@@ -16,14 +33,16 @@ function App() {
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" onChange={handleChange} />
-        <button>
+        <input type="text" onChange={handleChange} value={state.task} />
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {state.taskList.map((item) => {
+            return (<li>{item}</li>)
+          })}
         </ul>
       </div>
     </div>
