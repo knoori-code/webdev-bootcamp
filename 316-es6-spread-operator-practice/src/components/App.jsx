f305983c77b0cc957b75102a3992273faf7dd71d
knoori-code <knoori3@gmail.com>
2024-03-18 20:01:12 -0400

    Create handleChange function and task state variable

--- 316-es6-spread-operator-practice/src/components/App.jsx
+++ 316-es6-spread-operator-practice/src/components/App.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [task, setTask] = useState("");
+
+  function handleChange(event) {
+    const { value } = event.target;
+    
+    setTask(value);
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
+        <input type="text" onChange={handleChange} />
         <button>
           <span>Add</span>
         </button>
