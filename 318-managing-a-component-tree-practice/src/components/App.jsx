f7da4ea31a07f18ef84146df9383f8fffcdda7c5
knoori-code <knoori3@gmail.com>
2024-03-23 19:00:10 -0400

    Pass handleChange and addItem function to inputText component

--- 318-managing-a-component-tree-practice/src/components/App.jsx
+++ 318-managing-a-component-tree-practice/src/components/App.jsx
@@ -31,7 +31,7 @@ function App() {
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <InputArea />
+      <InputArea onType={handleChange} onButtonClick={addItem} />
       <div>
         <ul>
           {items.map((todoItem, index) => (
--- 318-managing-a-component-tree-practice/src/components/InputArea.jsx
+++ 318-managing-a-component-tree-practice/src/components/InputArea.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function InputArea() {
+function InputArea(props) {
   return (
     <div className="form">
-      <input onChange={handleChange} type="text" value={inputText} />
-      <button onClick={addItem}>
+      <input onChange={props.onType} type="text" value={inputText} />
+      <button onClick={props.onButtonClick}>
         <span>Add</span>
       </button>
     </div>
