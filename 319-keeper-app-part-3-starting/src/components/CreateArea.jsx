6ac57efbf0e0755f982a6faf73181530186731c8
knoori-code <knoori3@gmail.com>
2024-03-24 16:34:33 -0400

    Create handleChange function

--- 319-keeper-app-part-3-starting/src/components/CreateArea.jsx
+++ 319-keeper-app-part-3-starting/src/components/CreateArea.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 function CreateArea() {
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+  }
+  
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input name="title" placeholder="Title" onChange={handleChange} />
+        <textarea name="content" placeholder="Take a note..." rows="3" onChange={handleChange} />
         <button>Add</button>
       </form>
     </div>
