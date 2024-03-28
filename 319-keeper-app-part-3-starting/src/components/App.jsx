c94aa91811d9d4cd8f45451252ee5b45b3c83d0e
knoori-code <knoori3@gmail.com>
2024-03-27 20:04:49 -0400

    Create handleClick function and pass as prop to Note component

--- 319-keeper-app-part-3-starting/src/components/App.jsx
+++ 319-keeper-app-part-3-starting/src/components/App.jsx
@@ -13,12 +13,16 @@ function App() {
     });
   }
 
+  function handleClick(index) {
+
+  }
+
   return (
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
       {notes.map((note, index) => {
-        return <Note key={index} title={note.title} content={note.content} />;
+        return <Note key={index} id={index} title={note.title} content={note.content} onDelete={handleClick} />;
       })}
 
       <Footer />
